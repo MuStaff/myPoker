c366490da27a48fd054902ec4af783d564611964
Teggour <teggourmalik@gmail.com>
2021-09-06 18:21:00 +0300

    add

--- src/Poker/Poker.js
+++ src/Poker/Poker.js
@@ -1,109 +1,87 @@
-import React from 'react';
-// import {StyledPoker} from "./styled";
-
 // Start game (shuffle deck)
-function shuffler(arr){
-    let j, temp;
-    for(let i = arr.length - 1; i > 0; i--){
-        j = Math.floor(Math.random()*(i + 1));
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-    }
-    return arr;
+function shuffler(arr) {
+  let j, temp;
+  for (let i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
 }
 
-// Utils
-function numConv(numb) {
-    return Math.floor(numb/13);
+// create array 0-51 (deck)
+const deck = shuffler(Array.apply(null, {length: 52}).map(Number.call, Number))
+
+// convert number to suit number
+function convertToSuit(numb) {
+  return Math.floor(numb / 13);
 }
 
+// convert numbers from array to card numbers array
 function convertToCardNum(arr) {
-    return arr.map(num => num % 13)
+  return arr.map((num) => num % 13);
 }
 
+// check cards to Flush (one suit)
 function isFlush(arr) {
-    const setSuit = new Set();
+  const setSuit = new Set();
 
-    arr.forEach(card => {
-        setSuit.add(numConv(card))
-    })
+  arr.forEach((card) => {
+    setSuit.add(convertToSuit(card));
+  });
 
-    return (setSuit.size === 1 ? true : false)
+  return setSuit.size === 1 ? true : false;
 }
 
+// check cards to Straight (in series)
 function isStraight(arr) {
-    for()
-}
+  const setStep = new Set();
 
-function oneValue(arr) {
-    const setValue = arr.reduce((acc, curr) => {
-        numConv(curr)
-    }, {});
-
-    arr.forEach(card => {
-        setSuit.add(Math.floor(card/13))
+  convertToCardNum(arr)
+    .sort((a, b) => {
+      return a - b;
     })
+    .reduce((acc, cur, index) => {
+      if (index !== 0) {
+        setStep.add(cur - acc);
+        acc = cur;
+      } else acc = cur;
+    });
 
-    return (setSuit.size === 1 ? true : false)
+  return setStep.size === 1 ? true : false;
 }
 
-function biggerCard() {
-
+// create object by the occurrences of cards
+function occurrences(arr) {
+  return convertToCardNum(arr).reduce((acc, curr) => {
+    acc[curr] = (acc[curr] || 0) + 1;
+    return acc;
+  }, {});
 }
 
-const Poker = () => {
-    const countPlayers = 2;
-    const players = {};
-    const mainHand = [];
-    const deck = Array.from(Array(51).keys());
-    const randDeck = shuffler(deck);
-
-    console.log(randDeck[0])
-    console.log(numConv(randDeck[0]))
-
-    // function
-    function findWinCombo(array) {
-        let sortArr = array.sort((a,b) => {
-            return a - b;
-        })
-
-        if (true) console.log(sortArr.slice(-5));
-        // if () ;
-        // if () ;
-        // if () ;
-        // if () ;
-        // if () ;
-
-
-        return
-    }
-
-    // randomizer for array
-
-
-
-    // create players hands
-    for(let j = 0; j < 2; j++) {
-        for(let i = 0; i < countPlayers; i++) {
-            players[i] = players[i] ? [...players[i], randDeck.pop()] : [randDeck.pop()]
-        }
-    }
-
-    // create main hand
-    for(let j = 0; j < 5; j++) {
-        mainHand.push(randDeck.pop())
-    }
-
-    // find combination
-    // findWinCombo([...mainHand, ...players[0]])
-    // findWinCombo([...mainHand, ...players[1]])
-
-    return (
-        <div>
-
-        </div>
-    );
+// create all unique combinations with hand (5 of 7)
+const combine = (arr, count) => {
+  if (arr.length === count) return [arr];
+  else if (count === 0) return [[]];
+  else
+    return [
+      ...combine(arr.slice(1), count - 1).map((c) => [arr[0], ...c]),
+      ...combine(arr.slice(1), count),
+    ];
 };
 
-export default Poker;
+// check arr to Four of a kind
+function isFourOfAKind(arr) {
+  return Object.values(occurrences(arr)).includes(4)
+}
+
+// check arr to Full House
+function isFullHouse(arr) {
+  return Object.values(occurrences(arr)).includes(3) &&  Object.values(occurrences(arr)).includes(2)
+}
+
+// check arr to Full House
+function isThreeOfAKind(arr) {
+  return Object.values(occurrences(arr)).includes(3)
+}
